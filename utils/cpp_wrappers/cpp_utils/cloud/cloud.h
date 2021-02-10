3ff7c25d9ac0eac4dbc9c06b591432e75c115ea3
Aritra Sen <dino.tiger.lion@gmail.com>
2021-02-10 12:59:31 +0530

    compute

--- utils/cpp_wrappers/cpp_utils/cloud/cloud.h
+++ utils/cpp_wrappers/cpp_utils/cloud/cloud.h
@@ -147,6 +147,33 @@ PointXYZ max_point(std::vector<PointXYZ> points);
 PointXYZ min_point(std::vector<PointXYZ> points);
 
 
+struct PointCloud
+{
+
+	std::vector<PointXYZ>  pts;
+
+	// Must return the number of data points
+	inline size_t kdtree_get_point_count() const { return pts.size(); }
+
+	// Returns the dim'th component of the idx'th point in the class:
+	// Since this is inlined and the "dim" argument is typically an immediate value, the
+	//  "if/else's" are actually solved at compile time.
+	inline float kdtree_get_pt(const size_t idx, const size_t dim) const
+	{
+		if (dim == 0) return pts[idx].x;
+		else if (dim == 1) return pts[idx].y;
+		else return pts[idx].z;
+	}
+
+	// Optional bounding-box computation: return false to default to a standard bbox computation loop.
+	//   Return true if the BBOX was already computed by the class and returned in "bb" so it can be avoided to redo it again.
+	//   Look at bb.size() to find out the expected dimensionality (e.g. 2 or 3 for point clouds)
+	template <class BBOX>
+	bool kdtree_get_bbox(BBOX& /* bb */) const { return false; }
+
+};
+
+
 
 
 
